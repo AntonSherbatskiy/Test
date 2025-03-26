57e74e436add21b8d350e97d58dbf458405e62ed
AntonSherbatskiy <sherbatsskiy@gmail.com>
2025-03-26 23:21:42 +0200

    Add users route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("Hello world");
 });
 
+app.get("/users", (req, res) => {
+    res.json([{ id: 1, first_name: "Anton" }]);
+});
+
 app.listen(3000, () => {
     console.log("Server is started");
 })
